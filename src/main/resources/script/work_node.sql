e52597f774e50fc556807ca131bb6190fdd511f3
zhaopengcheng <zhaopengcheng@idwzx.com>
2018-12-21 22:52:49 +0800

    optimize

--- src/main/resources/script/work_node.sql
+++ src/main/resources/script/work_node.sql
@@ -1,11 +1,12 @@
-DROP TABLE IF EXISTS `work_node`;
+DROP TABLE IF EXISTS `worker_node`;
 
-CREATE TABLE `work_node` (
+CREATE TABLE `worker_node` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'auto increment id',
-  `IP` varchar(64) NOT NULL COMMENT 'host name',
+  `IP` varchar(128) NOT NULL COMMENT 'host name',
   `PORT` varchar(64) NOT NULL COMMENT 'port',
-  `LAUNCH_DATE` date NOT NULL COMMENT 'launch date',
-  `MODIFIED` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'modified time',
-  `CREATED` timestamp NOT NULL COMMENT 'created time',
+  `WORK_NODE_ID` int(11) NOT NULL COMMENT 'work node id',
+  `LAUNCH_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'launch date',
+  `STATUS` varchar(3) NOT NULL COMMENT 'server status, on or off',
+  `LAST_UPDATE_TIME` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'last update time',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='DB work node ID Assigner for UID Generator';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='DB WorkerID Assigner for UID Generator';
